e956b1fae319652f9d968e619ff452f08600d5d5
Pizza <wilderpizza99@gmail.com>
2024-05-27 21:15:21 +0200

    added an license

---
+++ LICENSE.md
@@ -0,0 +1,8 @@
+GPL-3.0 License
+
+This work is licensed under the GNU General Public License v3.0 only.
+You can find a copy of this license at:
+http://www.gnu.org/licenses/gpl-3.0.html
+
+If you didn't receive a copy of the license and are unable to access it
+at the URL above, you can contact the copyright holder.
--- src/main/java/com/github/WildePizza/Blizzity.java
+++ src/main/java/com/github/WildePizza/Blizzity.java
@@ -1,10 +1,16 @@
 package com.github.WildePizza;
 
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.IOException;
+
 public class Blizzity {
-    static String serverUrl = "http://localhost:8080";
-    public static void main(String[] args) {
+    static final boolean debug = false;
+    static String serverUrl = "http://localhost:8443";
+    public static void main(String[] args) throws GitAPIException, IOException {
+        String publicIP = (String) new VariablesBuilder().withGithubFile("https://github.com/Blizzity/BlizzityVariables/blob/main/variables.dat").build().getVariable("publicIP");
         Spark.run();
-        API api = new API(serverUrl);
+        API api = new API(debug ? serverUrl : serverUrl.replace("localhost", publicIP));
         GUI gui = new GUI(api);
         gui.open();
     }
