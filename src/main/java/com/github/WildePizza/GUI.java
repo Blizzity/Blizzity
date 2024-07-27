e4375d721770de713877414682e5bf8cca25340d
Pizza <wilderpizza99@gmail.com>
2024-07-27 22:53:20 +0200

    Update GUI.java

--- src/main/java/com/github/WildePizza/GUI.java
+++ src/main/java/com/github/WildePizza/GUI.java
@@ -126,7 +126,7 @@ public class GUI {
     private MediaView
             mediaView,
             mediaViewClone;
-    public static MappedJFXPanel jfxPanel;
+    public static ResizablePanel jfxPanel;
     private JIconButton exportButton;
     private JIconButton shareButton;
     ImageView imageView;
@@ -161,7 +161,7 @@ public class GUI {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         if ((screenWidth > screenSize.getWidth() || screenHeight > screenSize.getHeight() || sizeMultiplier != 1) && autoScale)
             sizeMultiplier = Math.min(screenSize.getHeight()/(screenHeight+100), screenSize.getWidth()/(screenWidth+100));
-        jfxPanel = new MappedJFXPanel();
+        jfxPanel = new ResizablePanel();
         ScreenListener.addMouseListener(jfxPanel, 0, 40);
 
         darkenBackground = new Rectangle(screenWidth*sizeMultiplier, screenHeight*sizeMultiplier);
@@ -492,7 +492,6 @@ public class GUI {
         init();
         if (variables.getVariable("key") != null || Blizzity.offlineMode) {
             showContentPanel((String) variables.getVariable("key"));
-//            addTitleBarPanel(api.admin((String) variables.getVariable("key")));
         } else {
             addTitleBarPanel(false);
             showLoginPanel();
@@ -1058,7 +1057,7 @@ public class GUI {
                 frame.add(jfxPanel);
             }
         });
-        jfxPanel.setPreferredSize(new Dimension((int) (screenWidth * sizeMultiplier), (int) (960 * sizeMultiplier)));
+        jfxPanel.setSize(screenWidth * sizeMultiplier, 960 * sizeMultiplier);
         frame.pack();
 //
 //
